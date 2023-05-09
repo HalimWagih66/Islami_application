d5665c02d553903de088a523ba5f4dc0f7d6bc56
shabir pjr <halemwagih6@gmail.com>
2023-05-09 13:45:27 +0300

    create screen layout

---
+++ lib/layout screen/layout screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:islami_application/layout%20screen/quran_Tap/quran_tap.dart';
+import 'package:islami_application/layout%20screen/radio_Tap/radio_tap.dart';
+import 'package:islami_application/layout%20screen/tasbeh_Tap/tasbeh_Tap.dart';
+
+import 'hadeth_Tap/hadeth_Tap.dart';
+
+class LayoutScreen extends StatefulWidget{
+  static const routeName = "LayoutScreen";
+
+  @override
+  State<LayoutScreen> createState() => _LayoutScreenState();
+}
+
+class _LayoutScreenState extends State<LayoutScreen> {
+  int selectedItemIndex = 0;
+  List<Widget> taps = [
+    QuranTap(),
+    HadethTap(),
+    TasbehTap(),
+    RadioTap(),
+  ];
+  List<String> title = [
+    "Quran",
+    "Hadeth",
+    "Tasbeh",
+    "Radio"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/layout screen/background_home_screen.png"),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title[selectedItemIndex]),
+        ),
+        body: taps[selectedItemIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedItemIndex,
+          onTap: (index){
+            selectedItemIndex = index;
+            setState(() {
+
+            });
+          },
+          items: [
+            BottomNavigationBarItem(backgroundColor: Theme.of(context).primaryColor,label: "Quran",icon: ImageIcon(AssetImage("assets/images/layout screen/icons BottomNavigationBar/quran.png"),)),
+            BottomNavigationBarItem(backgroundColor: Theme.of(context).primaryColor,label: "Hadeth",icon: ImageIcon(AssetImage("assets/images/layout screen/icons BottomNavigationBar/hadeth.png"),)),
+            BottomNavigationBarItem(backgroundColor: Theme.of(context).primaryColor,label: "Tasbeh",icon: ImageIcon(AssetImage("assets/images/layout screen/icons BottomNavigationBar/sebha.png"),)),
+            BottomNavigationBarItem(backgroundColor: Theme.of(context).primaryColor,label: "Radio",icon: ImageIcon(AssetImage("assets/images/layout screen/icons BottomNavigationBar/radio.png"),)),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'layout screen/layout screen.dart';
+
 void main() {
   runApp(MyApplication());
 }
@@ -7,9 +9,30 @@ class MyApplication extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
-
+        LayoutScreen.routeName:(context)=>LayoutScreen(),
       },
+      initialRoute: LayoutScreen.routeName,
+      theme: ThemeData(
+        primaryColor: Color(0xffB7935F),
+       scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          titleTextStyle: TextStyle(
+            color: Color(0xff242424),
+            fontSize: 30,
+            fontWeight: FontWeight.w500,
+          ),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          elevation: 0,
+           selectedItemColor: Color(0xff242424),
+          unselectedItemColor: Color(0xffFFFFFF),
+        )
+      ),
     );
   }
 
